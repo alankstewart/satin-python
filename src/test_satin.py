97d9ba083ef87a820003be391f7fe1b197088afb
Alan Stewart <alankstewart@gmail.com>
2023-12-26 20:42:14 +1100

    Simplify test function

--- src/test_satin.py
+++ src/test_satin.py
@@ -27,18 +27,11 @@ csv_file_path = os.path.join(script_directory, 'satin.csv')
 )
 def test_gaussian_calculation(input_power, small_signal_gain, saturation_intensity, output_power,
                               log_output_power_divided_by_input_power, output_power_minus_input_power):
-    input_power = int(input_power)
-    small_signal_gain = float(small_signal_gain)
-    saturation_intensity = int(saturation_intensity)
-    output_power = float(output_power)
-    log_output_power_divided_by_input_power = float(log_output_power_divided_by_input_power)
-    output_power_minus_input_power = float(output_power_minus_input_power)
-
-    for gaussian in gaussian_calculation(input_power, small_signal_gain):
-        if gaussian.saturation_intensity == saturation_intensity:
-            assert _round_up(gaussian.output_power) == output_power
+    for gaussian in gaussian_calculation(int(input_power), float(small_signal_gain)):
+        if gaussian.saturation_intensity == float(saturation_intensity):
+            assert _round_up(gaussian.output_power) == float(output_power)
             assert _round_up(
-                gaussian.log_output_power_divided_by_input_power()) == log_output_power_divided_by_input_power
-            assert _round_up(gaussian.output_power_minus_input_power()) == output_power_minus_input_power
+                gaussian.log_output_power_divided_by_input_power()) == float(log_output_power_divided_by_input_power)
+            assert _round_up(gaussian.output_power_minus_input_power()) == float(output_power_minus_input_power)
         else:
             pass

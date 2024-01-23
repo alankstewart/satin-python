399f7ea48613784e4266bb5764128e745f554b4a
Alan Stewart <alankstewart@gmail.com>
2024-01-23 21:30:11 +1100

    Combine two functions into one

--- src/satin.py
+++ src/satin.py
@@ -130,22 +130,15 @@ def _calculate_output_power(input_power, small_signal_gain, saturation_intensity
 
         output_power += output_intensity * EXPR * r
 
-    return Gaussian(input_power, output_power, saturation_intensity)
-
-
-def _calculate_output_power_alternate(input_power, small_signal_gain, saturation_intensity):
-    input_intensity = 2 * input_power / AREA
-    expr2 = saturation_intensity * small_signal_gain / 32000 * DZ
-
-    output_power = sum(
-        (
-            reduce(
-                lambda output_intensity, j: output_intensity * (
-                        1 + expr2 / (saturation_intensity + output_intensity) - EXPR1[j]
-                ), range(INCR), input_intensity * math.exp(-2 * r ** 2 / RAD2),
-            ) * EXPR * r for r in (i * DR for i in range(int(0.5 / DR)))
-        )
-    )
+    # output_power = sum(
+    #     (
+    #         reduce(
+    #             lambda output_intensity, j: output_intensity * (
+    #                     1 + expr2 / (saturation_intensity + output_intensity) - EXPR1[j]
+    #             ), range(INCR), input_intensity * math.exp(-2 * r ** 2 / RAD2),
+    #         ) * EXPR * r for r in (i * DR for i in range(int(0.5 / DR)))
+    #     )
+    # )
 
     return Gaussian(input_power, output_power, saturation_intensity)
 
